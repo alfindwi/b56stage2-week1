37c930629616444f92f333a988b49dcf3a9e5b95
unknown <alvindwi146@gmail.com>
2024-10-01 01:07:08 +0700

    remove url apiV2

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,17 +2,9 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const baseURL = import.meta.env.VITE_BACKEND_URL;
-const baseURLV2 = import.meta.env.VITE_BACKEND_URLV2;
 
 export const apiV1 = axios.create({
   baseURL: `${baseURL}/api/v1`,
-  headers: {
-    Authorization: `Bearer ${Cookies.get("token")}`,
-  },
-});
-
-export const apiV2 = axios.create({
-  baseURL: `${baseURL}/api/v2`,
   headers: {
     Authorization: `Bearer ${Cookies.get("token")}`,
   },
