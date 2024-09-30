b40731d90e22c26e2c21d51f786f889fa4f45460
unknown <alvindwi146@gmail.com>
2024-10-01 02:03:55 +0700

    change env

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 const baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const apiV1 = axios.create({
-  baseURL: `${baseURL}/api/v1`,
+  baseURL: `${baseURL}api/v1`,
   headers: {
     Authorization: `Bearer ${Cookies.get("token")}`,
   },
