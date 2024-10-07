19eb97b91a1ee4b53c3fdbff20718ad7315e8851
unknown <alvindwi146@gmail.com>
2024-10-07 11:34:30 +0700

    change code in app.tsx and _protected.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import Cookies from "js-cookie"
-import { UserStoreDTO } from "./features/auth/types/dto/dto"
-import { useAppDispatch } from "./hooks/use-store"
-import { apiV1 } from "./libs/api"
-import { AppRoute } from "./routes"
-import { setUser } from "./store/auth-slice"
-import { useEffect } from "react"
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+import { UserStoreDTO } from "./features/auth/types/dto/dto";
+import { useAppDispatch, useAppSelector } from "./hooks/use-store";
+import { apiV1 } from "./libs/api";
+import { AppRoute } from "./routes";
+import { setUser } from "./store/auth-slice";
 
 function App() {
   const dispatch = useAppDispatch();
 
+  const user = useAppSelector((state) => state.auth);
+
   async function getCurrentUser() {
-    const response = await apiV1.get<null, { data: UserStoreDTO }>(
-      "/auth/check",
-      {
-        headers: {
-          Authorization: `Bearer ${Cookies.get("token")}`,
-        },
+    try {
+      const token = Cookies.get("token");
+      if(!token) {
+        return;
       }
-    );
 
-    dispatch(setUser(response.data));
+      const response = await apiV1.get<null, {data: UserStoreDTO}>("/auth/check", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
 
+      dispatch(setUser(response.data));
+    }catch(error){
+      console.log(error)
+    }
   }
 
   useEffect(() => {
-    getCurrentUser();
-  }, []);
+    if(!user.id) {
+      getCurrentUser();
+    }
+  }, [user.id, dispatch]);
 
   return <AppRoute />;
 }
 
+export default App;
 
-
-export default App
--- src/routes/_protected.tsx
+++ src/routes/_protected.tsx
@@ -1,39 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-import Cookies from "js-cookie";
 import { Navigate, Outlet } from "react-router-dom";
-import { UserStoreDTO } from "../features/auth/types/dto/dto";
-import { useAppDispatch, useAppSelector } from "../hooks/use-store";
-import { apiV1 } from "../libs/api";
-import { setUser } from "../store/auth-slice";
+import { useAppSelector } from "../hooks/use-store";
 
 export function ProtectedRoute() {
   const user = useAppSelector((state) => state.auth);
-  const dispatch = useAppDispatch();
 
-  async function getCurrentUser() {
-    const response = await apiV1.get<null, { data: UserStoreDTO }>(
-      "/auth/check",
-      {
-        headers: {
-          Authorization: `Bearer ${Cookies.get("token")}`,
-        },
-      }
-    );
+  if (!user.id) return <Navigate to={"/login"} />;
 
-    dispatch(setUser(response.data));
-
-    return response.data;
-  }
-
-  
-  const { isLoading } = useQuery({
-    queryKey: ["currentUser"],
-    queryFn: getCurrentUser,
-  });
-
-  if (isLoading) return null;
-
-  if (user.id && user.role === "MEMBER") return <Outlet />;
+  if (user.role === "MEMBER") return <Outlet />;
 
   return <Navigate to={"/login"} />;
 }
+
